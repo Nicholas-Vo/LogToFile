fbdbf4326e46d389d8c63dd9227d7f6364bbb80b
Nick V <nvoss@me.com>
2020-12-15 15:01:58 -0600

    General code cleanup

--- LogToFile/src/logToFile/EventHandlers.java
+++ LogToFile/src/logToFile/EventHandlers.java
@@ -19,13 +19,15 @@ import org.bukkit.event.player.PlayerTakeLecternBookEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.inventory.meta.BookMeta;
 
-public class EventHandlers extends Utils implements Listener {
+public class EventHandlers implements Listener {
 	public File dataFolder;
-	public static File file;
-	public static YamlConfiguration yml;
+	public File file;
+	public YamlConfiguration yml;
+	private LogToFile plugin;
 
 	public EventHandlers(LogToFile logToFile) {
 		logToFile.getServer().getPluginManager().registerEvents(this, logToFile);
+		plugin = logToFile;
 		dataFolder = LogToFile.plugin.getDataFolder();
 		file = new File(dataFolder, "config.yml");
 		yml = YamlConfiguration.loadConfiguration(file);
@@ -40,7 +42,7 @@ public class EventHandlers extends Utils implements Listener {
 
 	@EventHandler
 	public void onProjectile(ProjectileHitEvent e) {
-		if (yml.getBoolean("logProjectiles") == true) {
+		if (yml.getBoolean("logProjectiles")) {
 			if (e != null && e.getHitEntity() != null) {
 				EntityType hitBlock = e.getHitEntity().getType();
 				if (hitBlock == EntityType.ARMOR_STAND || hitBlock == EntityType.ITEM_FRAME) {
@@ -48,8 +50,8 @@ public class EventHandlers extends Utils implements Listener {
 						Player shooter = (Player) e.getEntity().getShooter();
 						String hit = hitBlock.getName().replace("_", " ");
 
-						logToFile(shooter.getName() + " shot a projectile at an " + hit + " at "
-								+ copyableLocation(e.getHitEntity().getLocation()), "projectiles");
+						plugin.logToFile(shooter.getName() + " shot a projectile at an " + hit + " at "
+								+ plugin.copyableLocation(e.getHitEntity().getLocation()), "projectiles");
 					}
 				}
 			}
@@ -58,14 +60,14 @@ public class EventHandlers extends Utils implements Listener {
 
 	@EventHandler
 	public void onCreeperExplosion(EntityExplodeEvent e) {
-		if (yml.getBoolean("logCreeperProvoke") == true) {
+		if (yml.getBoolean("logCreeperProvoke")) {
 			if (e.getEntityType() == EntityType.CREEPER) {
 				List<Entity> nearby = e.getEntity().getNearbyEntities(10, 5, 10);
 				for (Entity entity : nearby) {
 					if (entity instanceof Player) {
-						logToFile("Creeper exploded at " + copyableLocation(e.getLocation()), "creeperProvoke");
-						logToFile("- Nearby: " + entity.getName(), "creeperProvoke");
-						logSpace("creeperProvoke");
+						plugin.logToFile("Creeper exploded at " + plugin.copyableLocation(e.getLocation()), "creeperProvoke");
+						plugin.logToFile("- Nearby: " + entity.getName(), "creeperProvoke");
+						plugin.logSpace("creeperProvoke");
 					}
 				}
 			}
@@ -74,39 +76,39 @@ public class EventHandlers extends Utils implements Listener {
 
 	@EventHandler
 	public void onBookTake(PlayerTakeLecternBookEvent e) {
-		if (yml.getBoolean("logLecternTake") == true) {
+		if (yml.getBoolean("logLecternTake")) {
 			Player player = e.getPlayer();
 			BookMeta book = (BookMeta) e.getBook().getItemMeta();
 			if (book != null) {
-				logToFile(player.getName() + " removed \"" + book.getTitle() + "\" by " + book.getAuthor() + " from "
-						+ copyableLocation(e.getLectern().getLocation()), "lecterntake");
+				plugin.logToFile(player.getName() + " removed \"" + book.getTitle() + "\" by " + book.getAuthor() + " from "
+						+ plugin.copyableLocation(e.getLectern().getLocation()), "lecterntake");
 			}
 		}
 	}
 
 	@EventHandler // Log getting on a horse
 	public void onHorseTake(VehicleEnterEvent e) {
-		if (yml.getBoolean("logHorsemounts") == true) {
+		if (yml.getBoolean("logHorsemounts")) {
 			if (e.getVehicle() instanceof Horse) {
-				logToFile(e.getEntered().getName() + " mounted a horse at "
-						+ copyableLocation(e.getVehicle().getLocation()), "horsemounts");
+				plugin.logToFile(e.getEntered().getName() + " mounted a horse at "
+						+ plugin.copyableLocation(e.getVehicle().getLocation()), "horsemounts");
 			}
 		}
 	}
 
 	@EventHandler // Log nametag use
 	public void onNameTag(PlayerInteractEntityEvent e) {
-		if (yml.getBoolean("logNametagUse") == true) {
+		if (yml.getBoolean("logNametagUse")) {
 			if (e.getPlayer().getItemInHand().getType() == Material.NAME_TAG) {
 				Entity entity = e.getRightClicked();
 				String tag = e.getPlayer().getItemInHand().getItemMeta().getDisplayName();
-				logToFile(e.getPlayer().getName() + " named " + entity.getName() + " at "
-						+ copyableLocation(entity.getLocation()) + ": " + tag, "nametaguse");
+				plugin.logToFile(e.getPlayer().getName() + " named " + entity.getName() + " at "
+						+ plugin.copyableLocation(entity.getLocation()) + ": " + tag, "nametaguse");
 			}
 		}
 	}
-
-	public static void saveYml() {
+	
+	public void saveYml() {
 		try {
 			yml.save(file);
 		} catch (IOException e) {
--- LogToFile/src/logToFile/LogToFile.java
+++ LogToFile/src/logToFile/LogToFile.java
@@ -1,14 +1,22 @@
 package logToFile;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LogToFile extends JavaPlugin implements Listener {
 	public static LogToFile plugin;
-	public static String PLUGIN_VERSION = "1.0.0";
+	public static String PLUGIN_VERSION = "1.0.5";
 	Logger log = Bukkit.getLogger();
 
 	public void onEnable() {
@@ -20,5 +28,57 @@ public class LogToFile extends JavaPlugin implements Listener {
 
 	public void onDisable() {
 		this.log.info("Disabled LogToFile " + PLUGIN_VERSION + ".");
+	}
+	
+	// Location String without commas
+	public String copyableLocation(Location l) {
+		return l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ() + " in \"" + l.getWorld().getName() + "\"";
+	}
+
+	public String locationToString(Player p) {
+		Location l = p.getLocation();
+		return "x" + l.getBlockX() + ", y" + l.getBlockY() + ", z" + l.getBlockZ();
+	}
+
+	private Date date = new Date();
+	private SimpleDateFormat niceLookingDate = new SimpleDateFormat("yyyy-MM-dd");
+	private String format = niceLookingDate.format(date);
+
+	public void logSpace(String fileName) {
+		try {
+			File dataFolder = (Bukkit.getPluginManager().getPlugin("LogToFile")).getDataFolder();
+			if (!dataFolder.exists())
+				dataFolder.mkdir();	
+			File saveTo = new File((Bukkit.getPluginManager().getPlugin("LogToFile")).getDataFolder(),
+					fileName + ".txt");
+			if (!saveTo.exists())
+				saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("");
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void logToFile(String message, String fileName) {
+		try {
+			File dataFolder = (Bukkit.getPluginManager().getPlugin("LogToFile")).getDataFolder();
+			if (!dataFolder.exists())
+				dataFolder.mkdir();
+			File saveTo = new File((Bukkit.getPluginManager().getPlugin("LogToFile")).getDataFolder(),
+					fileName + ".txt");
+			if (!saveTo.exists())
+				saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("[" + format.toString() + "] " + message);
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
